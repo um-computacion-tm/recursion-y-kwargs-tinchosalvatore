b8451c9e10538995c4ee1845d616d0357aaa1d10
tinchosalvatore <martingsalvatore@gmail.com>
2024-04-20 11:17:56 -0300

    version final buscar_datos

--- buscar_datos.py
+++ buscar_datos.py
@@ -27,35 +27,6 @@ database = {
     }
 }
 
-import unittest
-
-def buscar_datos(*args, **kwargs):
-    for key, value in kwargs.items():
-        for k, v in value.items():
-            if v in args:
-                return key
-    return None
-
-database = {
-    "per1": {
-        "nombre1": "Pablo",
-        "nombre2": "Diego",
-        "apellido1": "Ruiz",
-        "apellido2": "Picasso"
-    },
-    "per2": {
-        "nombre1": "Elio",
-        "apellido1": "Anci"
-    },
-    "per3": {
-        "nombre1": "Elias",
-        "nombre2": "Marcos",
-        "nombre3": "Luciano",
-        "apellido1": "Marcelo",
-        "apellido2": "Gonzalez"
-    }
-}
-
 datos = []
 for i in range(5):
     dato = input("Ingrese los datos a buscar (o 'fin' para terminar): ")
